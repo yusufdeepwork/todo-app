0b6fad9aa1252c6d052a8f808cb3761f71b13488
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-05-29 18:57:55 +0300

    add TodoHeader component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import TodoList from "./components/TodoList/TodoList";
 import "./App.css";
 import Form from "./components/Form/Form";
-
+import TodoHeader from "./components/TodoHeader/TodoHeader";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -50,6 +50,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <TodoHeader/>
         <Form
           userInput={this.state.userInput}
           onInputChange={this.onInputChange}
